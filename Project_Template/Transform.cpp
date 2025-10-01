46a15f2a42c7edc0b4f58afd578fca658f805727
PerniShoes <marek2001lenss@gmail.com>
2025-10-01 17:28:50 +0200

    Added explanation to translation

--- Project_Template/Transform.cpp
+++ Project_Template/Transform.cpp
@@ -16,7 +16,7 @@ void Transform::Push()const
 }
 void Transform::Apply() const
 {
-
+    // Position.x/y is based on middle of the sprite. So I translate it by half and a bit. So units move "onto" cursor, not "into" it
     glTranslatef(position.x,position.y+(hitboxWidth/2.0f * abs(scale.y)-(25.0f*abs(scale.y))),0);
     glScalef(scale.x, scale.y, 0);
     glRotatef(angle,0,0,1);
@@ -41,6 +41,7 @@ Rectf Transform::GetHitbox() const
 {
     float hitboxOffsetX{(hitboxWidth / 2.0f) * abs(scale.x)};
     float hitboxOffsetY{(hitboxWidth / 2.0f) * abs(scale.y)};
+    // -(25.0f) explanation above in Transform::Apply()
     return Rectf{position.x - hitboxOffsetX,position.y - (25.0f * abs(scale.y)),hitboxWidth * abs(scale.x),hitboxWidth * abs(scale.y)};
 }
 
